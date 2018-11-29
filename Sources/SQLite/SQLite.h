b584a71292a0d8bac60b41a15d9c0bc3337d62ba
EzlyJohnnie <a81658804@hotmail.com>
2018-11-30 10:37:37 +1300

    Fix bridging heading import issue

--- Sources/SQLite/SQLite.h
+++ Sources/SQLite/SQLite.h
@@ -3,4 +3,8 @@
 FOUNDATION_EXPORT double SQLiteVersionNumber;
 FOUNDATION_EXPORT const unsigned char SQLiteVersionString[];
 
+#if __has_include("SQLite-Bridging.h")
+#import "SQLite-Bridging.h"
+#else
 #import <SQLite/SQLite-Bridging.h>
+#endif
